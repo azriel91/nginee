09893e2fd20a5f0cd270beaaf670667001268a1c
Azriel Hoh <azriel91@gmail.com>
2020-05-30 11:11:10 +1200

    First approach for iced winit as an event handler that compiles.

--- crate/event_loop/src/event_handler.rs
+++ crate/event_loop/src/event_handler.rs
@@ -10,16 +10,21 @@ use crate::EventHandlerResult;
 #[cfg(feature = "rate_limit")]
 use crate::RateLimit;
 
+type HandlerTask<'f, Context, E> =
+    Pin<Box<dyn Future<Output = (Context, EventHandlerResult<E>)> + 'f>>;
+
 /// Wrapper type for event handler logic.
-pub struct EventHandler<E> {
+pub struct EventHandler<E, Context = ()> {
     /// Event handler logic.
-    fn_handler_logic: Box<dyn EventHandlerLogic<E>>,
+    fn_handler_logic: Box<dyn EventHandlerLogic<E, Context>>,
+    /// Context item.
+    context: Option<Context>,
     #[cfg(feature = "rate_limit")]
     /// Rate to limit this event handler's execution.
     pub rate_limit: Option<RateLimit>,
 }
 
-impl<E> EventHandler<E>
+impl<E> EventHandler<E, ()>
 where
     E: Error + Send + 'static,
 {
@@ -28,21 +33,51 @@ where
     /// # Parameters
     ///
     /// * `handler_logic`: The logic to run.
-    pub fn new<FnFut, Fut>(fn_handler_logic: FnFut) -> Self
+    pub fn new<FnFut, Fut>(mut fn_handler_logic: FnFut) -> Self
     where
-        Fut: Future<Output = EventHandlerResult<E>> + Send + 'static,
+        Fut: Future<Output = EventHandlerResult<E>> + 'static,
         FnFut: FnMut() -> Fut + 'static,
     {
-        let fn_handler_logic = {
-            let event_handler_logic = EventHandlerLogicBasic {
-                fn_handler_logic,
-                marker: PhantomData,
-            };
-            Box::new(event_handler_logic)
-        };
+        let fn_handler_logic = Box::new(FnHandlerWrapper {
+            fn_fut: move |_: ()| {
+                let fut = fn_handler_logic();
+                async move { ((), fut.await) }
+            },
+            marker: PhantomData,
+        });
 
         Self {
             fn_handler_logic,
+            context: Some(()),
+            #[cfg(feature = "rate_limit")]
+            rate_limit: None,
+        }
+    }
+}
+
+impl<E, Context> EventHandler<E, Context>
+where
+    E: Error + Send + 'static,
+{
+    /// Returns a new `EventHandler`.
+    ///
+    /// # Parameters
+    ///
+    /// * `context`: The context item used to construct the event handler logic.
+    /// * `handler_logic`: The logic to run.
+    pub fn new_with_context<FnFut, Fut>(context: Context, fn_handler_logic: FnFut) -> Self
+    where
+        Fut: Future<Output = (Context, EventHandlerResult<E>)> + 'static,
+        FnFut: FnMut(Context) -> Fut + 'static,
+    {
+        let fn_handler_logic = Box::new(FnHandlerWrapper {
+            fn_fut: fn_handler_logic,
+            marker: PhantomData,
+        });
+
+        Self {
+            fn_handler_logic,
+            context: Some(context),
             #[cfg(feature = "rate_limit")]
             rate_limit: None,
         }
@@ -57,16 +92,27 @@ where
 
     /// Runs the event handler logic.
     pub async fn run(&mut self) -> EventHandlerResult<E> {
-        self.fn_handler_logic.handler_task().await
+        if let Some(context) = self.context.take() {
+            let (context, result) = self.fn_handler_logic.handler_task(context).await;
+            self.context = Some(context);
+
+            result
+        } else {
+            unreachable!()
+        }
     }
 }
 
 #[cfg_attr(tarpaulin, skip)]
-impl<E> Debug for EventHandler<E> {
+impl<E, Context> Debug for EventHandler<E, Context>
+where
+    Context: Debug,
+{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut debug_struct = f.debug_struct("EventHandler");
 
         debug_struct.field("fn_handler_logic", &"..");
+        debug_struct.field("context", &self.context);
 
         #[cfg(feature = "rate_limit")]
         debug_struct.field("rate_limit", &self.rate_limit);
@@ -75,26 +121,22 @@ impl<E> Debug for EventHandler<E> {
     }
 }
 
-trait EventHandlerLogic<E> {
-    fn handler_task(
-        &mut self,
-    ) -> Pin<Box<dyn Future<Output = EventHandlerResult<E>> + Send + 'static>>;
-}
-
-struct EventHandlerLogicBasic<FnFut, Fut> {
-    fn_handler_logic: FnFut,
+struct FnHandlerWrapper<FnFut, Fut> {
+    fn_fut: FnFut,
     marker: PhantomData<Fut>,
 }
 
-impl<E, FnFut, Fut> EventHandlerLogic<E> for EventHandlerLogicBasic<FnFut, Fut>
+trait EventHandlerLogic<E, Context> {
+    fn handler_task<'f>(&mut self, context: Context) -> HandlerTask<'f, Context, E>;
+}
+
+impl<E, Context, FnFut, Fut> EventHandlerLogic<E, Context> for FnHandlerWrapper<FnFut, Fut>
 where
     E: Send + 'static,
-    Fut: Future<Output = EventHandlerResult<E>> + Send + 'static,
-    FnFut: FnMut() -> Fut,
+    Fut: Future<Output = (Context, EventHandlerResult<E>)> + 'static,
+    FnFut: FnMut(Context) -> Fut + 'static,
 {
-    fn handler_task(
-        &mut self,
-    ) -> Pin<Box<dyn Future<Output = EventHandlerResult<E>> + Send + 'static>> {
-        Box::pin((self.fn_handler_logic)())
+    fn handler_task<'f>(&mut self, context: Context) -> HandlerTask<'f, Context, E> {
+        Box::pin((self.fn_fut)(context))
     }
 }
---
+++ crate/iced/src/lib.rs
@@ -0,0 +1,13 @@
+//! Provides `iced` integration with `nginee_event_loop`.
+
+#![deny(
+    anonymous_parameters,
+    arithmetic_overflow,
+    array_into_iter,
+    missing_debug_implementations,
+    missing_docs
+)]
+
+pub use crate::iced_winit::IcedWinit;
+
+mod iced_winit;
